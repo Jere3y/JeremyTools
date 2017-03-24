06d0d4245917539310a5c45b7d7929f704d97af9
Jeremy <tianyurui@gmail.com>
2017-03-24 19:33:32 +0800

    更新地图路线功能，更加完善。

---
+++ app/src/main/java/jeremy/com/adapter/BusResultRecyclerAdapter.java
@@ -0,0 +1,10 @@
+package jeremy.com.adapter;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by Xin on 2017/3/24 0024,18:30.
+ */
+
+public class BusResultRecyclerAdapter extends RecyclerView.Adapter {
+}
